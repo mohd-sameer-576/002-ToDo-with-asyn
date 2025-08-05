8270c4b657d4c124b0d06c6f8c431266ff259265
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-08-05 21:55:47 +0530

    Refactor task management and add remove functionality
    Refactored the addTask function to fetch input value internally and clear the input after successful addition. Added removeTask function with event listener for removing tasks. Improved fetchTask to clear the list before rendering and fixed event handling for add and remove buttons.

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 let input = document.querySelector("#input");
 let addbtn = document.querySelector("#add");
 let listcontainer = document.querySelector(".listcontainer");
-fetchTask()
-addbtn.addEventListener("click", function(){
-    let value = input.value;
-    console.log(value);
-    addTask(value)
-    input.value = ""
-    fetchTask()
-})
+let removebtn = document.querySelector(".remove");
+let editbtn = document.querySelector(".edit");
+
+
+addbtn.addEventListener("click", addTask);
 let API = 'https://68921701447ff4f11fbf29de.mockapi.io/api/v1/todos'
 async function fetchTask(){
     let fetchingdata =await fetch(API);
     let data = await fetchingdata.json();
-
-    data.forEach(element => {
-        let div = document.createElement("div");
-        div.className = "task";
-        div.innerHTML = `
-        <p>${element.text}</p>
-        <div class="butoncontainer">
-            <button class="remove">Remove</button>
-            <button class="edit">Edit</button>
-        </div>
-        `
-        listcontainer.append(div);
-    });
+    if(data){
+        listcontainer.innerHTML = ""
+        data.forEach(element => {
+            let div = document.createElement("div");
+            div.className = "task";
+            div.innerHTML = `
+            <p>${element.text}</p>
+            <div class="butoncontainer">
+                <button class="remove">Remove</button>
+                <button class="edit">Edit</button>
+            </div>
+            `
+            listcontainer.append(div);
+        });
+    }
 }
 
-async function addTask(value){
+async function addTask(){
+    let value = input.value;
+    console.log(value);
+
     let data = await fetch(API, {
         method: "POST",
         headers: {
@@ -36,4 +38,31 @@ async function addTask(value){
         },
         body: JSON.stringify({text: value})
     })
+    if(data.status === 201){
+        input.value = ""
+        fetchTask()
+    }else{
+        console.log("Error")
+    }
 }
+fetchTask()
+
+async function removeTask(){
+    let data = await fetch(`${API}/${id}`, {
+        method: "DELETE"
+    })
+    if(data.status === 200){
+        fetchTask()
+    }else{
+        console.log("Error")
+    }
+}
+removebtn.addEventListener('click', removeTask)
+
+
+
+
+
+
+
+
