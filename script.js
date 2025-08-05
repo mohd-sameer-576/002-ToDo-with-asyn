0bb8191553767737fc45913f694eaa68894e56fe
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-08-05 20:52:29 +0530

    Implement dynamic task fetching and adding
    Removed hardcoded task from index.html and updated script.js to fetch tasks from the API and render them dynamically. Added functionality to add new tasks via the API and refresh the task list upon addition.

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 let input = document.querySelector("#input");
 let addbtn = document.querySelector("#add");
-
+let listcontainer = document.querySelector(".listcontainer");
+fetchTask()
 addbtn.addEventListener("click", function(){
     let value = input.value;
     console.log(value);
+    addTask(value)
+    input.value = ""
+    fetchTask()
 })
 let API = 'https://68921701447ff4f11fbf29de.mockapi.io/api/v1/todos'
-async function addTask(){
-    let data =await fetch(API);
-    console.log(await data.json())
+async function fetchTask(){
+    let fetchingdata =await fetch(API);
+    let data = await fetchingdata.json();
+
+    data.forEach(element => {
+        let div = document.createElement("div");
+        div.className = "task";
+        div.innerHTML = `
+        <p>${element.text}</p>
+        <div class="butoncontainer">
+            <button class="remove">Remove</button>
+            <button class="edit">Edit</button>
+        </div>
+        `
+        listcontainer.append(div);
+    });
 }
-addTask()
+
+async function addTask(value){
+    let data = await fetch(API, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({text: value})
+    })
+}
